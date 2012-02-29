3d684412841c0c2f95dec43c9d18fe29ce412384
Tonni Tølbøll Lund Aagesen <tonni@toelboell-lund.dk>
2012-02-29 16:05:36 +0100

    Add support for disabling verification of those homegrown SSL certificates you use

--- lib/redmine_post_action_hooks.rb
+++ lib/redmine_post_action_hooks.rb
@@ -1,9 +1,11 @@
+require "uri"
 require "net/https"
 
 # See list of hooks: http://www.redmine.org/projects/redmine/wiki/Hooks_List
 
 class RedminePostActionHooks < Redmine::Hook::Listener
   @@dispatch_url = nil
+  @@ssl_verify_none = nil
 
   def controller_issues_new_after_save(context = { })
     unless context[:issue].is_private
@@ -31,9 +33,10 @@ class RedminePostActionHooks < Redmine::Hook::Listener
   private
 
   def load_options
-    unless @@dispatch_url
+    if @@dispatch_url.nil? || @@ssl_verify_none.nil?
       options = YAML::load(File.open(File.join(Rails.root, 'config', 'redmine_post_action_hooks.yml')))
       @@dispatch_url = options[Rails.env]['url']
+      @@ssl_verify_none = options[Rails.env]['ssl_verify_none']
     end
   end
 
@@ -43,8 +46,25 @@ class RedminePostActionHooks < Redmine::Hook::Listener
     if @@dispatch_url
       context[:user] = context[:user].attributes.except("auth_source_id", "salt", "hashed_password") if context[:user]
       context[:assignee] = context[:assignee].attributes.except("auth_source_id", "salt", "hashed_password") if context[:assignee]
-      res = Net::HTTP.post_form(URI.parse(@@dispatch_url), { "payload" => context.to_json })
-      RAILS_DEFAULT_LOGGER.warn "Plugin redmine_post_action_hooks: Failed to dispath information, server returned status #{res.code}: #{res.msg}" unless res.class == Net::HTTPSuccess
+
+      begin
+        uri = URI.parse(@@dispatch_url)
+        
+        http = Net::HTTP.new(uri.host, uri.port)
+        if uri.scheme == "https"
+          http.use_ssl = true
+          http.verify_mode = OpenSSL::SSL::VERIFY_NONE if @@ssl_verify_none
+        end
+
+        post = Net::HTTP::Post.new(uri.request_uri)
+        post.set_form_data({ "payload" => context.to_json })
+
+        res = http.request(post)
+        RAILS_DEFAULT_LOGGER.warn "Plugin redmine_post_action_hooks: Failed to dispath information, server returned status #{res.code}: #{res.msg}" unless res === Net::HTTPSuccess
+      rescue Exception => e
+        RAILS_DEFAULT_LOGGER.warn "Plugin redmine_post_action_hooks: Caught an Exception: #{e.class} with message \"#{e.message}\""
+      end
+
     else
       RAILS_DEFAULT_LOGGER.warn("Plugin redmine_post_action_hooks: Unable to read URL from #{Rails.root}/config/redmine_post_action_hooks.yml")
     end
